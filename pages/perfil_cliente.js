c9c84f27670c57787afa1a5b6edb0b1288c54a3a
Karen1501 <karencsj0391@gmail.com>
2020-09-03 23:56:10 -0500

    perfil cliente responsivo

--- pages/perfil_cliente.js
+++ pages/perfil_cliente.js
@@ -39,10 +39,10 @@ function PerfilCliente() {
                 </a>
               </List>
               <List className={styles.item}>
-              <Link href="/mensaje_fotografa" passHref>
-                <a Mis Mensajes>
-                  {"Mis Mensajes"}
-                </a>
+                <Link href="/mensaje_fotografa" passHref>
+                  <a Mis Mensajes>
+                    {"Mis Mensajes"}
+                  </a>
                 </Link>
               </List>
               <List className={styles.item}>
@@ -56,11 +56,7 @@ function PerfilCliente() {
             <p className={styles.title}>¡BIENVENIDO!</p>
 
             <div className={styles.card_wrapper}>
-              <Card
-                className={styles.insideCard}
-                hoverable
-                style={{ width: 380, height: 400 }}
-              >
+              <Card className={styles.insideCard} hoverable>
                 <h3 className={styles.pTittle}> Hasta el momento tienes: </h3>
                 <img
                   className={styles.imgCard}
@@ -77,11 +73,7 @@ function PerfilCliente() {
                 </div>
               </Card>
 
-              <Card
-                hoverable
-                style={{ width: 380, height: 400 }}
-                className={styles.insideCard}
-              >
+              <Card hoverable className={styles.insideCard}>
                 <h3 className={styles.pTittle}>Calificación de </h3>
                 <img
                   className={styles.imgCard}
@@ -118,20 +110,20 @@ function PerfilCliente() {
                 </svg>
               </Button>
               <Link href="/busqueda">
-              <Button className={styles.button}>
-                Buscar por categoría
-                <svg
-                  className={styles.icon}
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 32 32"
-                >
-                  <title>search</title>
-                  <path d="M31.008 27.231l-7.58-6.447c-0.784-0.705-1.622-1.029-2.299-0.998 1.789-2.096 2.87-4.815 2.87-7.787 0-6.627-5.373-12-12-12s-12 5.373-12 12 5.373 12 12 12c2.972 0 5.691-1.081 7.787-2.87-0.031 0.677 0.293 1.515 0.998 2.299l6.447 7.58c1.104 1.226 2.907 1.33 4.007 0.23s0.997-2.903-0.23-4.007zM12 20c-4.418 0-8-3.582-8-8s3.582-8 8-8 8 3.582 8 8-3.582 8-8 8z"></path>
-                </svg>
-              </Button>
+                <Button className={styles.button}>
+                  Buscar por categoría
+                  <svg
+                    className={styles.icon}
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 32 32"
+                  >
+                    <title>search</title>
+                    <path d="M31.008 27.231l-7.58-6.447c-0.784-0.705-1.622-1.029-2.299-0.998 1.789-2.096 2.87-4.815 2.87-7.787 0-6.627-5.373-12-12-12s-12 5.373-12 12 5.373 12 12 12c2.972 0 5.691-1.081 7.787-2.87-0.031 0.677 0.293 1.515 0.998 2.299l6.447 7.58c1.104 1.226 2.907 1.33 4.007 0.23s0.997-2.903-0.23-4.007zM12 20c-4.418 0-8-3.582-8-8s3.582-8 8-8 8 3.582 8 8-3.582 8-8 8z"></path>
+                  </svg>
+                </Button>
               </Link>
             </div>
           </Content>
